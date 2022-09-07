469371a2704ddd67da5940297dbade0207c860fc
Gustavo <gustavoquintero9105@gmail.com>
2022-09-07 12:01:43 -0300

    -

--- main.f90
+++ main.f90
@@ -6,16 +6,16 @@ Program main
     integer :: allocerr,iter,int_iter,i,kflag
     integer, parameter :: max_iter = 100,max_int_iter = 100
     real(kind=8), parameter :: alpha = 0.5d0,epsilon = 1.0d-7,delta=0.1d0
-    real(kind=8), allocatable :: xk(:),xtrial(:),faux(:),gaux(:),indices(:)
+    real(kind=8), allocatable :: xtrial(:),faux(:),indices(:)
     integer, allocatable :: Idelta(:)
-    real(kind=8) :: fxk,fxtrial,aux,opt_cond,sigma
+    real(kind=8) :: fxk,fxtrial,aux,opt_cond,sigma,gaux1,gaux2
     logical :: box
 
     ! COMMON SCALARS
     integer :: samples,q
 
     ! COMMON ARRAYS
-    real(kind=8),   pointer :: t(:), y(:)
+    real(kind=8),   pointer :: t(:),y(:),grad(:,:),xk(:)
 
     ! COMMON BLOCKS
     common /integerData/ samples,q
@@ -48,8 +48,8 @@ Program main
 
     ! Coded subroutines
 
-    coded(1:6)  = .true.  ! evalf, evalg, evalh, evalc, evaljac, evalhc
-    coded(7:11) = .false. ! evalfc,evalgjac,evalgjacp,evalhl,evalhlp
+    coded(1:5)  = .true.  ! evalf, evalg, evalh, evalc, evaljac, evalhc
+    coded(6:11) = .false. ! evalfc,evalgjac,evalgjacp,evalhl,evalhlp
 
 
     ! Upper bounds on the number of sparse-matrices non-null elements
@@ -80,7 +80,7 @@ Program main
     !==============================================================================
     iter = 0
 
-    xk = 1.0d0
+    xk(:) = 1.0d0
 
     box = .false.
 
@@ -104,14 +104,14 @@ Program main
     ! Sorting
     call DSORT(faux,indices,samples,kflag)
 
-    call mount_Idelta(faux,xk,n,delta,indices,Idelta,m)
+    call mount_Idelta(faux,xk,n,indices,delta,Idelta,m)
 
     ! Minimizing using ALGENCAN
 
     do
         iter = iter + 1
 
-        allocate(equatn(m),linear(m),lambda(m),stat=allocerr)
+        allocate(equatn(m),linear(m),lambda(m),grad(m,n-1),stat=allocerr)
 
         if ( allocerr .ne. 0 ) then
             write(*,*) 'Allocation error in main program'
@@ -121,6 +121,20 @@ Program main
         equatn(:) = .false.
         linear(:) = .false.
         lambda(:) = 0.0d0
+
+        x(1:n) = (/xk(1:n-1), 0.d0/)
+
+        do i = 1, m
+            gaux1 = model(x,Idelta(i),n) - y(Idelta(i))
+            gaux2 = (x(1) / x(3)) * t(Idelta(i)) * exp(-x(3) * t(Idelta(i))) + (x(1) / x(3)**2)
+
+            grad(i,1) = gaux1 * exp(gaux2) * ((1.0d0 / x(3)) * t(Idelta(i)) * exp(-x(3) * t(Idelta(i))) + &
+                        (1.0d0 / x(3)**2) * exp(-x(3) * t(Idelta(i))) - (1.0d0 / x(3)**2))
+    
+            grad(i,2) = gaux1 * (-1.0d0 / x(3)) * exp(-x(3) * t(Idelta(i))) - t(Idelta(i)) + 1.0d0 / x(3)
+
+            grad(i,3) = gaux1 * 
+        end do
 
         if (iter .ge. max_iter) exit
 
@@ -150,16 +164,14 @@ Program main
     !==============================================================================
     !
     !==============================================================================
-    subroutine mount_Idelta(f,x,n,delta,indices,Idelta,m)
+    subroutine mount_Idelta(f,x,n,indices,delta,Idelta,m)
         implicit none
 
         integer,        intent(in) :: n
         real(kind=8),   intent(in) :: delta,x(n),f(samples),indices(samples)
         integer,        intent(out) :: Idelta(samples),m
-        integer :: samples,q,i
+        integer :: i
         real(kind=8) :: fq
-
-        common /integerData/ samples,q
 
         Idelta(:) = 0
         fq = f(q)
@@ -188,33 +200,6 @@ Program main
     !==============================================================================
     !
     !==============================================================================
-    subroutine mount_grad(dFmin,x,n,indices)
-        implicit none
-
-        integer,        intent(in) :: n,indices(q)
-        real(kind=8),   intent(in) :: x(n)
-        real(kind=8),   intent(out) :: dFmin(n)
-        integer :: i,j
-        real(kind=8) :: gx
-
-        ! COMMON SCALARS
-        integer :: samples,p,q
-
-        ! COMMON ARRAYS
-        real(kind=8),   pointer :: t(:), y(:)
-
-        common /integerData/ samples,q
-        common /realVectorData/ t,y
-
-        dFmin(1:n) = 0.0d0
-                
-        do j = 1, q
-            gx = (model(x,indices(j),n) - y(indices(j)))
-            dFmin(1:n) = dFmin(1:n) + gx * (/((t(indices(j)) - t(samples))**i, i = 1, n)/)
-        enddo
-
-    end subroutine mount_grad
-
     function fi(x,i,n) result (res)
         implicit none
 
@@ -236,6 +221,9 @@ Program main
 
     end function fi
 
+    !==============================================================================
+    !
+    !==============================================================================
     function model(x,i,n) result(res)
         implicit none 
 
@@ -284,4 +272,266 @@ Program main
 
         close(10)
     end subroutine read_data
+
+    !==============================================================================
+    ! 
+    !==============================================================================
+    subroutine myevalf(n,x,f,flag)
+        implicit none
+
+        ! SCALAR ARGUMENTS
+        integer, intent(in) :: n
+        integer, intent(out) :: flag
+        real(kind=8), intent(out) :: f
+
+        ! ARRAY ARGUMENTS
+        real(kind=8), intent(in) :: x(n)
+
+        ! Compute objective function
+
+        flag = 0
+
+        f =  x(n)
+
+    end subroutine myevalf
+
+    !==============================================================================
+    ! 
+    !==============================================================================
+    subroutine myevalg(n,x,g,flag)
+        implicit none
+
+        ! SCALAR ARGUMENTS
+        integer, intent(in) :: n
+        integer, intent(out) :: flag
+
+        ! ARRAY ARGUMENTS
+        real(kind=8), intent(in) :: x(n)
+        real(kind=8), intent(out) :: g(n)
+
+        ! Compute gradient of the objective function
+
+        flag = 0
+
+        g(1:n-1) = 0.0d0
+        g(n)     = 1.0d0
+
+    end subroutine myevalg
+
+    !==============================================================================
+    ! 
+    !==============================================================================
+    subroutine myevalh(n,x,hrow,hcol,hval,hnnz,lim,lmem,flag)
+        implicit none
+
+        ! SCALAR ARGUMENTS
+        logical, intent(out) :: lmem
+        integer, intent(in) :: lim,n
+        integer, intent(out) :: flag,hnnz
+
+        ! ARRAY ARGUMENTS
+        integer, intent(out) :: hcol(lim),hrow(lim)
+        real(kind=8), intent(in)  :: x(n)
+        real(kind=8), intent(out) :: hval(lim)
+
+        ! Compute (lower triangle of the) Hessian of the objective function
+        flag = 0
+        lmem = .false.
+        hnnz = 0
+    end subroutine myevalh
+
+    !==============================================================================
+    ! 
+    !==============================================================================
+    subroutine myevalc(n,x,ind,c,flag)
+        implicit none
+
+        ! SCALAR ARGUMENTS
+        integer, intent(in) :: ind,n
+        integer, intent(out) :: flag
+        real(kind=8), intent(out) :: c
+
+        ! ARRAY ARGUMENTS
+        real(kind=8), intent(in) :: x(n)
+
+        real(kind=8) :: sigma
+        real(kind=8), pointer :: grad(:,:), xk(:)
+
+        common /realData/ sigma
+        common /arrayData/ grad, xk
+
+        ! Compute ind-th constraint
+        flag = 0
+
+        c = dot_product(x(1:n-1) - xk(1:n-1),grad(ind,1:n-1)) + (sigma * 0.5d0) * &
+            (norm2(x(1:n-1) - xk(1:n-1))**2) - x(n)
+
+    end subroutine myevalc
+
+    !==============================================================================
+    ! 
+    !==============================================================================
+    subroutine myevaljac(n,x,ind,jcvar,jcval,jcnnz,lim,lmem,flag)
+
+        implicit none
+
+        ! SCALAR ARGUMENTS
+        logical, intent(out) :: lmem
+        integer, intent(in) :: ind,lim,n
+        integer, intent(out) :: flag,jcnnz
+
+        ! ARRAY ARGUMENTS
+        integer, intent(out) :: jcvar(lim)
+        real(kind=8), intent(in) :: x(n)
+        real(kind=8), intent(out) :: jcval(lim)
+
+        real(kind=8) :: sigma
+        real(kind=8), pointer :: grad(:,:), xk(:)
+
+        integer :: i
+
+        common /realData/ sigma
+        common /arrayData/ grad, xk
+
+        flag = 0
+        lmem = .false.
+
+        jcnnz = n
+
+        if ( jcnnz .gt. lim ) then
+            lmem = .true.
+            return
+        end if
+
+        jcvar(1:n) = (/(i, i = 1, n)/)
+        jcval(1:n) = (/(grad(ind,i) + sigma * (x(i) - xk(i)), i = 1, n-1), -1.0d0/)
+
+    end subroutine myevaljac
+
+    !==============================================================================
+    ! 
+    !==============================================================================
+    subroutine myevalhc(n,x,ind,hcrow,hccol,hcval,hcnnz,lim,lmem,flag)
+
+        implicit none
+
+        ! SCALAR ARGUMENTS
+        logical, intent(out) :: lmem
+        integer, intent(in) :: ind,lim,n
+        integer, intent(out) :: flag,hcnnz
+
+        ! ARRAY ARGUMENTS
+        integer, intent(out) :: hccol(lim),hcrow(lim)
+        real(kind=8), intent(in) :: x(n)
+        real(kind=8), intent(out) :: hcval(lim)
+
+        flag =  -1
+
+    end subroutine myevalhc
+
+    ! ******************************************************************
+    ! ******************************************************************
+
+    subroutine myevalfc(n,x,f,m,c,flag)
+
+        implicit none
+
+        ! SCALAR ARGUMENTS
+        integer, intent(in) :: m,n
+        integer, intent(out) :: flag
+        real(kind=8), intent(out) :: f
+
+        ! ARRAY ARGUMENTS
+        real(kind=8), intent(in) :: x(n)
+        real(kind=8), intent(out) :: c(m)
+
+        flag = - 1
+
+    end subroutine myevalfc
+
+    ! ******************************************************************
+    ! ******************************************************************
+
+    subroutine myevalgjac(n,x,g,m,jcfun,jcvar,jcval,jcnnz,lim,lmem,flag)
+
+        implicit none
+
+        ! SCALAR ARGUMENTS
+        logical, intent(out) :: lmem
+        integer, intent(in) :: lim,m,n
+        integer, intent(out) :: flag,jcnnz
+
+        ! ARRAY ARGUMENTS
+        integer, intent(out) :: jcfun(lim),jcvar(lim)
+        real(kind=8), intent(in) :: x(n)
+        real(kind=8), intent(out) :: g(n),jcval(lim)
+
+        flag = - 1
+
+    end subroutine myevalgjac
+
+    ! ******************************************************************
+    ! ******************************************************************
+
+    subroutine myevalgjacp(n,x,g,m,p,q,work,gotj,flag)
+
+        implicit none
+
+        ! SCALAR ARGUMENTS
+        logical, intent(inout) :: gotj
+        integer, intent(in) :: m,n
+        integer, intent(out) :: flag
+        character, intent(in) :: work
+
+        ! ARRAY ARGUMENTS
+        real(kind=8), intent(in) :: x(n)
+        real(kind=8), intent(inout) :: p(m),q(n)
+        real(kind=8), intent(out) :: g(n)
+
+        flag = - 1
+
+    end subroutine myevalgjacp
+
+    ! ******************************************************************
+    ! ******************************************************************
+
+    subroutine myevalhl(n,x,m,lambda,sf,sc,hlrow,hlcol,hlval,hlnnz,lim,lmem,flag)
+
+        implicit none
+
+        ! SCALAR ARGUMENTS
+        logical, intent(out) :: lmem
+        integer, intent(in) :: lim,m,n
+        integer, intent(out) :: flag,hlnnz
+        real(kind=8), intent(in) :: sf
+
+        ! ARRAY ARGUMENTS
+        integer, intent(out) :: hlcol(lim),hlrow(lim)
+        real(kind=8), intent(in) :: lambda(m),sc(m),x(n)
+        real(kind=8), intent(out) :: hlval(lim)
+
+        flag = - 1
+
+    end subroutine myevalhl
+
+    ! ******************************************************************
+    ! ******************************************************************
+
+    subroutine myevalhlp(n,x,m,lambda,sf,sc,p,hp,goth,flag)
+
+        implicit none
+
+        ! SCALAR ARGUMENTS
+        logical, intent(inout) :: goth
+        integer, intent(in) :: m,n
+        integer, intent(out) :: flag
+        real(kind=8), intent(in) :: sf
+
+        ! ARRAY ARGUMENTS
+        real(kind=8), intent(in) :: lambda(m),p(n),sc(m),x(n)
+        real(kind=8), intent(out) :: hp(n)
+
+        flag = - 1
+
+    end subroutine myevalhlp
 end Program main
